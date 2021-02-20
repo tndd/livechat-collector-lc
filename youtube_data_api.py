cf7b3626c096f8931c616145fc15c511ba533643
tan <ttru702@gmail.com>
2021-02-20 14:36:07 +0900

    rename vars
    The name of "video" is likely to be confused with other data.

--- youtube_data_api.py
+++ youtube_data_api.py
@@ -19,7 +19,7 @@ def is_exist_youtube_data_api_search(channel_id):
     return os.path.exists(f"{SAVE_DIR_PATH_SEARCH}/{channel_id}.json")
 
 
-def get_videos_search_list_from_channel_id_part(channnel_id, next_page_token=None):
+def get_channel_search_list_from_channel_id_part(channnel_id, next_page_token=None):
     api_service_name = "youtube"
     api_version = "v3"
     api_key = os.environ.get("YOUTUBE_DATA_API_KEY")
@@ -43,41 +43,41 @@ def get_videos_search_list_from_channel_id_part(channnel_id, next_page_token=Non
     return response.get('nextPageToken', NEXT_PAGE_TOKEN_IS_EMPTY), response['items']
 
 
-def get_videos_search_list_from_channel_id(channel_id):
+def get_channel_search_list_from_channel_id(channel_id):
     if is_exist_youtube_data_api_search(channel_id):
         print(f"[SKIP]: SEARCH_LIST \"{channel_id}\" have already been downloaded.")
         return SEARCH_LIST_DOWNLOAD_IS_COMPLETED
 
     next_page_token = None
-    videos_search_items = []
+    channel_search_items = []
     while next_page_token != NEXT_PAGE_TOKEN_IS_EMPTY:
-        next_page_token, video_items = get_videos_search_list_from_channel_id_part(channel_id, next_page_token)
-        videos_search_items.extend(video_items)
+        next_page_token, channel_search_items = get_channel_search_list_from_channel_id_part(channel_id, next_page_token)
+        channel_search_items.extend(channel_search_items)
     print(f"[Downloaded]: SEARCH_LIST \"{channel_id}\"")
-    return videos_search_items
+    return channel_search_items
 
 
-def store_videos_search_list(channel_id, videos_search_items):
+def store_channel_search_list(channel_id, channel_search_items):
     with open(f"{SAVE_DIR_PATH_SEARCH}/{channel_id}.json", 'w') as f:
-        json.dump(videos_search_items, f, ensure_ascii=False, indent=4)
+        json.dump(channel_search_items, f, ensure_ascii=False, indent=4)
     print(f"[STORED]: SEARCH_LIST \"{channel_id}\"")
 
 
-def task_videos_search_list():
+def task_channel_search_list():
     # download channel_list's video data and store.
     with open('channel.json', 'r') as f:
         channel_items = json.load(f)
     for talent_name, channel_id in channel_items.items():
-        videos_search_items = get_videos_search_list_from_channel_id(channel_id)
+        channel_search_items = get_channel_search_list_from_channel_id(channel_id)
 
-        if videos_search_items == SEARCH_LIST_DOWNLOAD_IS_COMPLETED:
+        if channel_search_items == SEARCH_LIST_DOWNLOAD_IS_COMPLETED:
             continue
 
-        store_videos_search_list(channel_id, videos_search_items)
+        store_channel_search_list(channel_id, channel_search_items)
 
 
 def main():
-    task_videos_search_list()
+    task_channel_search_list()
 
 
 if __name__ == "__main__":
