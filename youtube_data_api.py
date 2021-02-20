eceeea1a1ecb523fb22701fe93b0bd907224ebca
tan <ttru702@gmail.com>
2021-02-20 13:49:43 +0900

    change to not download file that have already been downloaded.

--- youtube_data_api.py
+++ youtube_data_api.py
@@ -6,11 +6,17 @@ from dotenv import load_dotenv
 import googleapiclient.discovery
 import googleapiclient.errors
 
-EMPTY_TOKEN = 'empty_token'
+NEXT_PAGE_TOKEN_IS_EMPTY = 'empty_token'
+SEARCH_LIST_DOWNLOAD_IS_COMPLETED = 'search_list_is_downloaded'
+
 SAVE_DIR_PATH = '.youtube_data_api/search'
 
 load_dotenv('.env')
 os.makedirs(SAVE_DIR_PATH, exist_ok=True)
+
+
+def is_exist_youtube_data_api_search(channel_id):
+    return os.path.exists(f"{SAVE_DIR_PATH}/{channel_id}.json")
 
 
 def get_videos_search_list_part(channnel_id, next_page_token=None):
@@ -34,22 +40,29 @@ def get_videos_search_list_part(channnel_id, next_page_token=None):
     )
     response = request.execute()
 
-    return response.get('nextPageToken', EMPTY_TOKEN), response['items']
+    return response.get('nextPageToken', NEXT_PAGE_TOKEN_IS_EMPTY), response['items']
 
 
-def get_videos_search_list_part(channel_id):
+def get_videos_search_list(channel_id):
+    if is_exist_youtube_data_api_search(channel_id):
+        print(f"[SKIP]: SEARCH_LIST \"{channel_id}\" have already been downloaded.")
+        return SEARCH_LIST_DOWNLOAD_IS_COMPLETED
+
     next_page_token = None
     videos_items = []
-    while next_page_token != EMPTY_TOKEN:
-        print(next_page_token)
+    while next_page_token != NEXT_PAGE_TOKEN_IS_EMPTY:
         next_page_token, video_items = get_videos_search_list_part(channel_id, next_page_token)
         videos_items.extend(video_items)
+    print(f"[COMPLETE]: Downloaded SEARCH_LIST \"{channel_id}\"")
     return videos_items
 
 
 def main():
     channel_id = 'UCvaTdHTWBGv3MKj3KVqJVCw'
-    videos_items = get_videos_search_list_part(channel_id)
+    videos_items = get_videos_search_list(channel_id)
+
+    if videos_items == SEARCH_LIST_DOWNLOAD_IS_COMPLETED:
+        return
 
     with open(f"{SAVE_DIR_PATH}/{channel_id}.json", 'w') as f:
         json.dump(videos_items, f, ensure_ascii=False, indent=4)
