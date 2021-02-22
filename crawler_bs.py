e69560889a3bb6414799d114ef0286758df985fe
tan <ttru702@gmail.com>
2021-02-22 16:33:49 +0900

    add function store y_initial_data

--- crawler_bs.py
+++ crawler_bs.py
@@ -1,8 +1,13 @@
 import requests
 import re
 import json
+import os
 
 from bs4 import BeautifulSoup
+
+
+SAVE_DIR_PATH_Y_INITIAL_DATA = 'data/y_initial_data'
+os.makedirs(SAVE_DIR_PATH_Y_INITIAL_DATA, exist_ok=True)
 
 
 def get_y_initial_data_from_video_id(video_id):
@@ -17,7 +22,13 @@ def get_y_initial_data_from_video_id(video_id):
     return None
 
 
+def store_y_initial_data(video_id, y_initial_data):
+    with open(f"{SAVE_DIR_PATH_Y_INITIAL_DATA}/{video_id}.json", 'w') as f:
+        json.dump(y_initial_data, f, ensure_ascii=False, indent=4)
+    print(f"[STORED]: Y_INITIAL_DATA \"{video_id}\"")
+
+
 if __name__ == "__main__":
     video_id = 'AuOOuPTKC48'
     a = get_y_initial_data_from_video_id(video_id)
-    print(a)
+    store_y_initial_data(video_id, a)
