a5926f5a043aab4c315be5f7bcb6521390ba7141
tan <ttru702@gmail.com>
2021-02-22 17:01:48 +0900

    add function download y_initial_data from channel_id

--- crawler_bs.py
+++ crawler_bs.py
@@ -5,9 +5,20 @@ import os
 
 from bs4 import BeautifulSoup
 
+from youtube_data_api import get_video_ids_from_channel_id
+
 
 SAVE_DIR_PATH_Y_INITIAL_DATA = 'data/y_initial_data'
 os.makedirs(SAVE_DIR_PATH_Y_INITIAL_DATA, exist_ok=True)
+
+
+def is_exist_y_initial_data(video_id):
+    return os.path.exists(f"{SAVE_DIR_PATH_Y_INITIAL_DATA}/{video_id}.json")
+
+
+def clear_y_initial_data(video_id):
+    if is_exist_y_initial_data(video_id):
+        os.remove(f"{SAVE_DIR_PATH_Y_INITIAL_DATA}/{video_id}.json")
 
 
 def get_y_initial_data_from_video_id(video_id):
@@ -28,7 +39,21 @@ def store_y_initial_data(video_id, y_initial_data):
     print(f"[STORED]: Y_INITIAL_DATA \"{video_id}\"")
 
 
+def download_y_initial_datas_from_channel_id(channel_id):
+    video_ids = get_video_ids_from_channel_id(channel_id)
+    for video_id in video_ids:
+        if is_exist_y_initial_data(video_id):
+            print(f"[SKIP]: Y_INITIAL_DATA \"{video_id}\" have already been downloaded.")
+            continue
+        try:
+            y_initial_data = get_y_initial_data_from_video_id(video_id)
+            store_y_initial_data(video_id, y_initial_data)
+        except Exception as e:
+            clear_y_initial_data(video_id)
+            print(e)
+    print(f"[Completed]: Y_INITIAL_DATAS of \"{channel_id}\"")
+
+
 if __name__ == "__main__":
-    video_id = 'AuOOuPTKC48'
-    a = get_y_initial_data_from_video_id(video_id)
-    store_y_initial_data(video_id, a)
+    channel_id = 'UCvaTdHTWBGv3MKj3KVqJVCw'
+    download_y_initial_datas_from_channel_id(channel_id)
